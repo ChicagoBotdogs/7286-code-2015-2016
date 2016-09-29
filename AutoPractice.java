5c2d26f9d8a4bec38cc973d24a5c7e2bbd1dfc25
mbaltman <mbaltman@cps.edu>
2016-09-29 17:15:02 -0500

    Add files via upload

---
+++ AutoPractice.java
@@ -0,0 +1,65 @@
+package org.firstinspires.ftc.robotcontroller.external.samples;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is a K9 robot.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *
+ * Motor channel:  Left  drive motor:        "left motor"
+ * Motor channel:  Right drive motor:        "right motor"
+ * Servo channel:  Servo to raise/lower arm: "arm"
+ * Servo channel:  Servo to open/close claw: "claw"
+ *
+ * Note: the configuration of the servos is such that:
+ *   As the arm servo approaches 0, the arm position moves up (away from the floor).
+ *   As the claw servo approaches 0, the claw opens up (drops the game element).
+ */
+public class AutoPractice extends OpMode
+{
+    /* Public OpMode members. */
+    public DcMotor motor1;
+
+    /* Constructor */
+    public AutoPractice() {
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init() {
+        // save reference to HW Map
+
+        motor1=hardwareMap.dcMotor.get("motor_1");
+
+        motor1.setPower(.5);
+
+
+
+        motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+
+
+    }
+    public void loop() {
+    }
+    /***
+     *
+     * waitForTick implements a periodic delay. However, this acts like a metronome with a regular
+     * periodic tick.  This is used to compensate for varying processing times for each cycle.
+     * The function looks at the elapsed cycle time, and sleeps for the remaining time interval.
+     *
+     * @param periodMs  Length of wait cycle in mSec.
+     * @throws InterruptedException
+     */
+    public void waitForTick(long periodMs)  throws InterruptedException {
+
+    }
+}
