0da43623b8c75d08b522a5f3c19e11fa8aebc73f
mbaltman <mbaltman@cps.edu>
2016-10-11 17:26:29 -0500

    Add files via upload

--- AutoPractice.java
+++ AutoPractice.java
@@ -1,10 +1,7 @@
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 /**
  * This is NOT an opmode.
